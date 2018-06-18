22ebdd9d4d219ca9705d2b92785ed0717f053ea0
Destan Sarpkaya <destan@dorukdestan.com>
2018-06-18 16:01:52 +0300

    initial

---
+++ src/main/java/com/kodgemisi/course/ecommerce/EcommerceApplication.java
@@ -0,0 +1,14 @@
+package com.kodgemisi.course.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
+
+
+}
